4ffe73c7a52442e1df41758991afb7af2d74a30b
Jash Doshi <shirothegamer69@gmail.com>
2022-11-17 21:21:57 +0530

    landing section 1 added
    need to sort out reponsive

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,24 +2,18 @@ import React from 'react'
 import {useNavigate} from 'react-router-dom'
 import Navbar from '../components/Landing/Navbar/Navbar'
 import './Landing.css'
+
 function Landing() {
   const navigate = useNavigate()
+
   return (
-    <div className='landing'>
-      <Navbar />
-      <div className="landing-section1">
-        <div className="background">
-          <svg width="100%" height="120vmax" viewBox="0 0 1440 1771" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.4">
-              <circle cx="528" cy="566" r="1160" fill="white" stroke="#F3F3F4" stroke-width="90"/>
-              <circle cx="440" cy="478" r="1072" fill="white" stroke="#F3F3F4" stroke-width="90"/>
-              <circle cx="426.5" cy="496.5" r="882.5" fill="white" stroke="#F3F3F4" stroke-width="90"/>
-              <circle cx="445" cy="483" r="683" fill="white" stroke="#F3F3F4" stroke-width="90"/>
-              <circle cx="431.5" cy="469.5" r="509.5" fill="white" stroke="#F3F3F4" stroke-width="90"/>
-              <circle cx="425" cy="476" r="323" fill="white" stroke="#F3F3F4" stroke-width="90"/>
-            </g>
-            <circle cx="422.5" cy="469.5" r="195.5" fill="#FDCE02" stroke="white" stroke-width="20"/>
-          </svg>
+    <div className="landing-img-bg">
+      <div className='landing'>
+        <Navbar />
+        <div className="landing-section1">
+          <div className="content">
+            <h1>Restoring <br />your movement <br />with physiotherapy</h1>
+          </div>
         </div>
       </div>
     </div>
